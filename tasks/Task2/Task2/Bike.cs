de4ce0993673dabe6986b13a927c0778bf2e6a64
GeorgFarrenkopf <gfarrenkopf@gmx.at>
2016-06-13 17:43:14 +0200

    add project Task2

---
+++ tasks/Task2/Task2/Bike.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+	class bike
+	{
+		private int rahmenSize;
+		private string name;
+
+		public bike(int size, string name)
+		{
+			Size = size;
+			UpdateSize(size);
+		}
+
+		public int size
+		{
+			get
+			{
+				return rahmenSize;
+			}
+			set
+			{
+				if (value < 1) throw new Exception("Size muss > 0 sein");
+				rahmenSize = value;
+			}
+		}
+
+		public string Name
+		{
+			get
+			{
+				return name;
+			}            
+		}
+
+		public void UpdateName(string name)
+		{
+			if (string.IsNullOrWhiteSpace(name)) throw new Exception("Name darf nicht leer sein");
+			this.name = name;
+		}
+
+	}
+}
---
+++ tasks/Task2/Task2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+	class program
+	{
+		static void Main(string[] args)
+		{
+			try
+			{
+				var x = new bike ("KTM", 29);
+				Console.WriteLine(x.Name);
+				Console.WriteLine(x.Size);
+				x.UpdateName ("Scott");
+				Console.WriteLine (x.Name);
+			}
+		catch (Exception ex)
+			{
+				Console.WriteLine($"Error:{ex}");
+			}
+		}
+	}
+}
